21de583cb86bde5697d509889d80374ef9d9c948
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-13 13:54:29 +0530

    Register page and styling in Login footer

--- client/src/pages/common/Login/index.jsx
+++ client/src/pages/common/Login/index.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { Form } from "antd";
+import { Link } from "react-router-dom";
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <div className="card w-400 p-3">
         <div className="flex flex-col">
           <h1 className="text-2xl">Login</h1>
-          <Form layout="vertical">
+          <Form layout="vertical" onFinish={onFinish}>
             <Form.Item name="email" label="Email">
               <input type="text" />
             </Form.Item>
             <Form.Item name="password" label="Password">
               <input type="password" />
             </Form.Item>
-            <button type="submit" className="primary-contained-btn mt-2 w-100">
-              Login
-            </button>
+            <div className="flex flex-col gap-1">
+              <button
+                type="submit"
+                className="primary-contained-btn mt-2 w-100"
+              >
+                Login
+              </button>
+              <Link to="/register">Not a member? Register</Link>
+            </div>
           </Form>
         </div>
       </div>
--- client/src/pages/common/Register/index.jsx
+++ client/src/pages/common/Register/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 const Register = () => {
-  return <div>Register</div>;
+  const onFinish = () => {};
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="card w-400 p-3">
+        <div className="flex flex-col">
+          <h1 className="text-2xl">Register</h1>
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item name="name" label="Name">
+              <input type="text" />
+            </Form.Item>
+            <Form.Item name="email" label="Email">
+              <input type="text" />
+            </Form.Item>
+            <Form.Item name="password" label="Password">
+              <input type="password" />
+            </Form.Item>
+            <div className="flex flex-col gap-1">
+              <button
+                type="submit"
+                className="primary-contained-btn mt-2 w-100"
+              >
+                Register
+              </button>
+              <Link to="/login">Already a member? Login</Link>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;

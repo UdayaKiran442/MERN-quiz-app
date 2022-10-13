2e9cd881be10b224b6dd6c4292b2b26fdf103289
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-13 13:08:38 +0530

    Antd setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { Button } from "antd";
 function App() {
   return (
     <div className="App">
       <h1>Shey Quiz</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }

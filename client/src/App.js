29851cdf3e50cee181ccdbb94bf969b098c9eb96
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-13 13:18:12 +0530

    common styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import { Button } from "antd";
+import "./styleSheets/alignments.css";
+import "./styleSheets/textElements.css";
+import "./styleSheets/theme.css";
+
 function App() {
   return (
     <div className="App">
       <h1>Shey Quiz</h1>
-      <Button type="primary">Primary Button</Button>
     </div>
   );
 }

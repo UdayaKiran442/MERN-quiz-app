3c31930a72773a067bb60a149de415a8c27f1b12
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-14 09:34:36 +0530

    server setup and database connection

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error in connecting database"));
+db.once("open", () => console.log("Connected to database"));
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});

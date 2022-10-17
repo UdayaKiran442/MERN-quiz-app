bba36640af4c8e17affb9840539586d64597b55a
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-17 18:15:57 +0530

    Fetching exam list

--- client/src/pages/admin/Exams/index.jsx
+++ client/src/pages/admin/Exams/index.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageTitle from "../../../components/PageTitle";
 import { useNavigate } from "react-router-dom";
+import { message, Table } from "antd";
+import { useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../../../redux/loaderSlice";
+import { getAllExams } from "../../../api/exam";
 
 const Exams = () => {
   const navigate = useNavigate();
+  const [exams, setExams] = useState([]);
+  const dispatch = useDispatch();
+  const columns = [
+    {
+      title: "Exam Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Duration",
+      dataIndex: "duration",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+    },
+    {
+      title: "Total Marks",
+      dataIndex: "totalMarks",
+    },
+    {
+      title: "Passing Marks",
+      dataIndex: "passingMarks",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => (
+        <div className="flex gap-2">
+          <span
+            className="material-symbols-outlined"
+            onClick={() => {
+              navigate(`/admin/exams/edit/${record._id}`);
+            }}
+          >
+            edit
+          </span>
+          <span
+            className="material-symbols-outlined"
+            onClick={() => {
+              navigate(`/admin/exams/delete/${record._id}`);
+            }}
+          >
+            delete
+          </span>
+        </div>
+      ),
+    },
+  ];
+  const getExamsData = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await getAllExams();
+      dispatch(hideLoading());
+      if (response.success) {
+        setExams(response.data);
+        message.success(response.message);
+      } else {
+        message.error(response.error);
+      }
+    } catch (error) {
+      dispatch(hideLoading);
+      message.error(error.message);
+    }
+  };
+  useEffect(() => {
+    getExamsData();
+  }, []);
   return (
     <div>
       <div className="flex justify-between mt-2 items-end">
@@ -17,6 +88,7 @@ const Exams = () => {
           Add Exam
         </button>
       </div>
+      <Table columns={columns} dataSource={exams} />
     </div>
   );
 };
